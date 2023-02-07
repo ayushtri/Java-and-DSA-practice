81f60d45c2c3d8597ce9fb4371737408161c7f1d
Ayush Tripathi <tripathi.at02@gmail.com>
2023-02-07 15:22:15 +0530

    Java and Dsa

---
+++ src/com/company/DSA/mergeSort.java
@@ -0,0 +1,43 @@
+package com.company.DSA;
+
+public class mergeSort {
+    public static void divide(int[] arr, int si, int ei){
+        if(si>=ei) return;
+        int mid = si+((ei-si)/2);
+        divide(arr, si, mid);
+        divide(arr, mid+1, ei);
+        conquer(arr, si, mid, ei);
+    }
+
+    private static void conquer(int[] arr, int si, int mid, int ei) {
+        int[] merge = new int[ei-si+1];
+        int idx1 = si;
+        int idx2 = mid+1;
+        int x=0;
+        while(idx1<=mid && idx2<=ei){
+            if(arr[idx1]<=arr[idx2]){
+                merge[x++]=arr[idx1++];
+            }
+            else{
+                merge[x++]=arr[idx2++];
+            }
+        }
+        while(idx1<=mid){
+            merge[x++]=arr[idx1++];
+        }
+        while(idx2<=ei){
+            merge[x++]=arr[idx2++];
+        }
+        for(int i=0,j=si;i<merge.length;i++,j++){
+            arr[j]=merge[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,1,4,6,3,9,7,5,8};
+        divide(arr,0,arr.length-1);
+        for(int i : arr){
+            System.out.print(i+", ");
+        }
+    }
+}
